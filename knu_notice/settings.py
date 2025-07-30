030369d21fa8cb1be90025ff1e52e9054c964307
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-07-31 02:08:27 +0900

    test: git actions 크롤링 오류 해결 테스트

--- knu_notice/settings.py
+++ knu_notice/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,knu-notice-web.onrender.com').split(',')
 
 
 # Application definition
